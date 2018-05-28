7ca5d07fa131905779b8b16e9c5646d61800e69a
Anthony Young <anthony@aiitec.com>
2018-05-28 18:17:03 +0800

    修改写渠道的脚本

--- writeChannels.bat
+++ writeChannels.bat
@@ -8,18 +8,7 @@ set checkV2JarFilePath=%libPath%\CheckAndroidV2Signature.jar
 set walleJarFilePath=%libPath%\walle-cli-all.jar
 set outputFilePath=%currentPath%\channels
 set channelFilePath=%currentPath%\channel
-
-
-for /f "tokens=1,2 delims==" %%i in (signConfig.properties) do (
-if "%%i"=="sdkPath" set sdkPath=%%j
-if "%%i"=="keyPath" set keyPath=%%j
-if "%%i"=="alias" set alias=%%j
-if "%%i"=="keyPassword" set keyPassword=%%j
-if "%%i"=="storePassword" set storePassword=%%j
-if "%%i"=="filePath" set filePath=%%j
-
-)
-
+set filePath=currentPath\app_signed.apk
 
 :: 删除lib路径右边的空格
 :delLibPathRightSpace
@@ -28,19 +17,9 @@ if "%libPath:~-1%"==" " set libPath=%abc:~0,-1%&&goto delLibPathRightSpace
 
 set dropFile=%1
 if defined dropFile (set filePath=%dropFile%) 
-::echo 截取前5个字符：
-::echo %ifo:~0,5%
-::echo 截取最后5个字符：
-::echo %ifo:~-5%
-::echo 截取第一个到倒数第6个字符：
-::echo %ifo:~0,-5%
-::echo 从第4个字符开始，截取5个字符：
-::echo %ifo:~3,5%
-::echo 从倒数第14个字符开始，截取5个字符：
-::echo %ifo:~-14,5%
+
 set outputPath=%currentPath%\output\
 if not exist %outputPath% md %outputPath%
-for %%a in (%filePath%) do set fineName=%%~nxa
 
 
 ::获取libPath盘符
@@ -48,7 +27,7 @@ for %%a in (%filePath%) do set fineName=%%~nxa
 ::cd  %libPath%
 
 ::检查V2签名是否正确
-call java -jar %checkV2JarFilePath% %dropFile%
+call java -jar %checkV2JarFilePath% %filePath%
 
 
 ::写入渠道
@@ -56,7 +35,7 @@ call java -jar %checkV2JarFilePath% %dropFile%
 :: 创建输出文件夹
 if not exist %outputFilePath% md %outputFilePath%
 
-call java -jar %walleJarFilePath% batch -f %channelFilePath% %dropFile% %outputFilePath%
+call java -jar %walleJarFilePath% batch -f %channelFilePath% %filePath% %outputFilePath%
 
 
 if "%errorlevel%"=="0" (echo success,please press any key to exit) else (echo error) 
